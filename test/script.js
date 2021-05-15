fcd567eca4e25c42433c12a82346a2be8fa25120
kimberlychapman <63875440+kimberlychapman@users.noreply.github.com>
2021-05-15 16:53:47 -0500

    added more categories to rewards and set up right side panel

--- test/script.js
+++ test/script.js
@@ -67,6 +67,75 @@ function getCounter4(multiplier = 1) {
 document.getElementById('minus4').onclick = getCounter4(-1);
 
 document.getElementById('plus4').onclick = getCounter4(); 
+// 
+function getCounter5(multiplier = 1) {
+    return function () {
+       var counter5 = document.getElementById('counter5').innerHTML;
+       var parsed = parseInt(counter5);
+       var result5 = parsed + (multiplier * 1);
+       document.getElementById('counter5').innerHTML = result5;
+       if (result5 < 0){
+        document.getElementById('counter5').innerHTML = 0;
+    }
+    }
+}
+
+document.getElementById('minus5').onclick = getCounter5(-1);
+
+document.getElementById('plus5').onclick = getCounter5();
+// 
+
+function getCounter6(multiplier = 1) {
+    return function () {
+       var counter6 = document.getElementById('counter6').innerHTML;
+       var parsed = parseInt(counter6);
+       var result6 = parsed + (multiplier * 1);
+       document.getElementById('counter6').innerHTML = result6;
+       if (result6 < 0){
+        document.getElementById('counter6').innerHTML = 0;
+    }
+    }
+}
+
+document.getElementById('minus6').onclick = getCounter6(-1);
+
+document.getElementById('plus6').onclick = getCounter6();
+
+// 
+
+function getCounter7(multiplier = 1) {
+    return function () {
+       var counter7 = document.getElementById('counter7').innerHTML;
+       var parsed = parseInt(counter7);
+       var result7 = parsed + (multiplier * 1);
+       document.getElementById('counter7').innerHTML = result7;
+       if (result7 < 0){
+        document.getElementById('counter7').innerHTML = 0;
+    }
+    }
+}
+
+document.getElementById('minus7').onclick = getCounter7(-1);
+
+document.getElementById('plus7').onclick = getCounter7();
+
+// 
+
+function getCounter8(multiplier = 1) {
+    return function () {
+       var counter8 = document.getElementById('counter8').innerHTML;
+       var parsed = parseInt(counter8);
+       var result8 = parsed + (multiplier * 1);
+       document.getElementById('counter8').innerHTML = result8;
+       if (result8 < 0){
+        document.getElementById('counter8').innerHTML = 0;
+    }
+    }
+}
+
+document.getElementById('minus8').onclick = getCounter8(-1);
+
+document.getElementById('plus8').onclick = getCounter8();
 
 // document.getElementById('minus').onclick = function() {
 //     var counter = document.getElementById('counter').innerHTML;
