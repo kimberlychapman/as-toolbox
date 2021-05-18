80094e9c02cf1334e32b24d2ed20df81d3c2ccc8
kimberlychapman <63875440+kimberlychapman@users.noreply.github.com>
2021-05-18 14:17:17 -0500

    added images to shopping cart list and a clear cart button

--- shop/script.js
+++ shop/script.js
@@ -14,7 +14,23 @@
 //     document.getElementById('bey').style.display="none";
 // }
 
-
+// $('input[name="radio-btn"]').wrap('<div class="radio-btn"><i></i></div>');
+// $(".radio-btn").on('click', function () {
+//     var _this = $(this),
+//         block = _this.parent().parent();
+//     block.find('input:radio').attr('checked', false);
+//     block.find(".radio-btn").removeClass('checkedRadio');
+//     _this.addClass('checkedRadio');
+//     _this.find('input:radio').attr('checked', true);
+// });
+// $('input[name="check-box"]').wrap('<div class="check-box"><i></i></div>');
+// $.fn.toggleCheckbox = function () {
+//     this.attr('checked', !this.attr('checked'));
+// }
+// $('.check-box').on('click', function () {
+//     $(this).find(':checkbox').toggleCheckbox();
+//     $(this).toggleClass('checkedBox');
+// });
 
 function getCounter2(multiplier = 1) {
     return function () {
@@ -205,3 +221,7 @@ function getCounter9(multiplier = 1) {
 document.getElementById('minus9').onclick = getCounter9(-1);
 
 document.getElementById('plus9').onclick = getCounter9(); 
+
+function refreshPage(){
+    window.location.reload();
+}
